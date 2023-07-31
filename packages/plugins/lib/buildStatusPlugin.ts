07142c0e3863f9c1472ff45f85b86aeaf16a58ad
leegeunhyeok <dev.ghlee@gmail.com>
2023-08-01 04:23:59 +0900

    feat: add buildStatusPlugin

---
+++ packages/plugins/lib/buildStatusPlugin.ts
@@ -0,0 +1,42 @@
+import ora, { type Ora } from 'ora';
+import type { BuildResult } from 'esbuild';
+import type { PluginCreator, BuildStatusPluginConfig } from './types';
+
+export const createBuildStatusPlugin: PluginCreator<BuildStatusPluginConfig> = (
+  config,
+) => ({
+  name: 'build-status-plugin',
+  setup: (build): void => {
+    const { printSpinner, onStart, onResolve, onLoad, onEnd } = config;
+    let spinner: Ora | null = null;
+
+    build.onStart(() => {
+      if (printSpinner) {
+        spinner = ora({
+          color: 'green',
+          text: 'build in progress...',
+        });
+        spinner.start();
+      } else {
+        spinner = null;
+      }
+      onStart?.();
+    });
+
+    build.onResolve({ filter: /\*./ }, (args) => {
+      onResolve?.(args.path);
+      return null;
+    });
+
+    build.onLoad({ filter: /\*./ }, (args) => {
+      onLoad?.(args.path);
+      return null;
+    });
+
+    build.onEnd((result: BuildResult<{ write: false }>) => {
+      spinner?.succeed('done!');
+      spinner?.stop();
+      onEnd?.(result);
+    });
+  },
+});
--- packages/plugins/lib/index.ts
+++ packages/plugins/lib/index.ts
@@ -1,5 +1,11 @@
 import { createAssetRegisterPlugin } from './assetRegisterPlugin';
+import { createBuildStatusPlugin } from './buildStatusPlugin';
 import { createHermesTransformPlugin } from './hermesTransformPlugin';
 
-export { createAssetRegisterPlugin, createHermesTransformPlugin };
+export {
+  createAssetRegisterPlugin,
+  createBuildStatusPlugin,
+  createHermesTransformPlugin,
+};
+
 export type * from './types';
