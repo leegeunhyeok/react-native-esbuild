83049d1a7d7d5c0ef955aad3fe4e07e1f8d0feda
leegeunhyeok <dev.ghlee@gmail.com>
2023-08-02 06:11:40 +0900

    feat: enhance loading spinner

--- packages/plugins/lib/buildStatusPlugin/buildStatusPlugin.ts
+++ packages/plugins/lib/buildStatusPlugin/buildStatusPlugin.ts
@@ -8,14 +8,22 @@ export const createBuildStatusPlugin: PluginCreator<BuildStatusPluginConfig> = (
   name: 'build-status-plugin',
   setup: (build): void => {
     const { printSpinner, onStart, onResolve, onLoad, onEnd } = config;
+    // eslint-disable-next-line prefer-named-capture-group
+    const filter = /(.*?)/;
     let spinner: Ora | null = null;
+    let moduleLoaded = 0;
+
+    const updateStatusText = (): void => {
+      if (!spinner) return;
+      spinner.text = `build in progress... (loaded: ${moduleLoaded})`;
+    };
 
     build.onStart(() => {
-      if (printSpinner) {
-        spinner = ora({
-          color: 'green',
-          text: 'build in progress...',
-        });
+      moduleLoaded = 0;
+
+      if (printSpinner && !spinner) {
+        spinner = ora({ color: 'green' });
+        updateStatusText();
         spinner.start();
       } else {
         spinner = null;
@@ -23,18 +31,22 @@ export const createBuildStatusPlugin: PluginCreator<BuildStatusPluginConfig> = (
       onStart?.();
     });
 
-    build.onResolve({ filter: /\*./ }, (args) => {
+    build.onResolve({ filter }, (args) => {
       onResolve?.(args.path);
       return null;
     });
 
-    build.onLoad({ filter: /\*./ }, (args) => {
+    build.onLoad({ filter }, (args) => {
+      ++moduleLoaded;
+      updateStatusText();
       onLoad?.(args.path);
       return null;
     });
 
     build.onEnd((result: BuildResult<{ write: false }>) => {
-      spinner?.succeed('done!');
+      result.errors.length
+        ? spinner?.fail('failed!')
+        : spinner?.succeed('done!');
       spinner?.stop();
       onEnd?.(result);
     });
