ebc5e83d6c65306bf26659d9cabe67b24889156a
leegeunhyeok <dev.ghlee@gmail.com>
2023-08-15 15:56:16 +0900

    feat: implement symbolicate-middleware

--- packages/dev-server/lib/middlewares/symbolicate.ts
+++ packages/dev-server/lib/middlewares/symbolicate.ts
@@ -1,19 +1,52 @@
+import { parse } from 'node:url';
+import { BundleTaskSignal } from '@react-native-esbuild/core';
+import {
+  parseStackFromRawBody,
+  symbolicateStackTrace,
+} from '@react-native-esbuild/symbolicate';
 import { logger } from '../shared';
+import { parseBundleConfigFromSearchParams } from '../helpers';
 import type { DevServerMiddlewareCreator } from '../types';
 
 const TAG = 'symbolicate-middleware';
 
-export const createSymbolicateMiddleware: DevServerMiddlewareCreator = (
-  _context,
-) => {
-  return function symbolicateMiddleware(request, _response, next) {
+export const createSymbolicateMiddleware: DevServerMiddlewareCreator = ({
+  bundler,
+}) => {
+  return function symbolicateMiddleware(request, response, next) {
     if (!request.url) {
       logger.warn(`(${TAG}) request url is empty`);
       return next();
     }
 
-    // TODO
+    const { pathname } = parse(request.url, true);
+    if (!pathname?.endsWith('/symbolicate')) {
+      return next();
+    }
 
-    return next();
+    const stack = parseStackFromRawBody(request.rawBody);
+    const targetStack = stack.find(({ file }) => file.startsWith('http'));
+
+    if (!targetStack) {
+      throw new Error('unable to processing');
+    }
+
+    const { query } = parse(targetStack.file, true);
+    const bundleConfig = parseBundleConfigFromSearchParams(query);
+
+    bundler
+      .getSourcemap(bundleConfig)
+      .then(({ sourcemap }) => symbolicateStackTrace(sourcemap, stack))
+      .then((symbolicateResult) => {
+        response.writeHead(200).end(JSON.stringify(symbolicateResult));
+      })
+      .catch((errorOrSignal) => {
+        if (errorOrSignal === BundleTaskSignal.EmptyOutput) {
+          logger.error('bundle result is empty');
+        } else {
+          logger.error('symbolicate error', errorOrSignal as Error);
+        }
+        response.writeHead(500).end();
+      });
   };
 };
