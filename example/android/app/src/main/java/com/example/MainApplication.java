0e61e717ba6c0d371efbf1abdcfb11223135781b
leegeunhyeok <dev.ghlee@gmail.com>
2023-08-17 22:08:31 +0900

    feat: add native logger (android)

--- example/android/app/src/main/java/com/example/MainApplication.java
+++ example/android/app/src/main/java/com/example/MainApplication.java
@@ -1,16 +1,24 @@
 package com.example;
 
 import android.app.Application;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+import com.facebook.common.logging.FLog;
+import com.facebook.common.logging.LoggingDelegate;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.ReactMarker;
+import com.facebook.react.bridge.ReactMarkerConstants;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.facebook.soloader.SoLoader;
 import java.util.List;
 
-public class MainApplication extends Application implements ReactApplication {
+public class MainApplication extends Application implements ReactApplication, LoggingDelegate {
 
   private final ReactNativeHost mReactNativeHost =
       new DefaultReactNativeHost(this) {
@@ -52,11 +60,99 @@ public class MainApplication extends Application implements ReactApplication {
   @Override
   public void onCreate() {
     super.onCreate();
+    FLog.setLoggingDelegate(this);
     SoLoader.init(this, /* native exopackage */ false);
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
       // If you opted-in for the New Architecture, we load the native entry point for this app.
       DefaultNewArchitectureEntryPoint.load();
     }
     ReactNativeFlipper.initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
+  }
+
+  @Override
+  public void setMinimumLoggingLevel(int level) {
+    // noop
+  }
+
+  @Override
+  public int getMinimumLoggingLevel() {
+      return 0;
+  }
+
+  @Override
+  public boolean isLoggable(int level) {
+    return true;
+  }
+
+  @Override
+  public void v(String tag, String msg) {
+    Log.v("RNFLog", tag + " - " + msg);
+  }
+
+  @Override
+  public void v(String tag, String msg, Throwable tr) {
+    Log.v("RNFLog", tag + " - " + msg + " : " + tr.getMessage());
+  }
+
+  @Override
+  public void d(String tag, String msg) {
+    Log.d("RNFLog", tag + " - " + msg);
+  }
+
+  @Override
+  public void d(String tag, String msg, Throwable tr) {
+    Log.d("RNFLog", tag + " - " + msg + " : " + tr.getMessage());
+  }
+
+  @Override
+  public void i(String tag, String msg) {
+    Log.i("RNFLog", tag + " - " + msg);
+  }
+
+  @Override
+  public void i(String tag, String msg, Throwable tr) {
+    Log.i("RNFLog", tag + " - " + msg + " : " + tr.getMessage());
+  }
+
+  @Override
+  public void w(String tag, String msg) {
+    Log.w("RNFLog", tag + " - " + msg);
+  }
+
+  @Override
+  public void w(String tag, String msg, Throwable tr) {
+    Log.w("RNFLog", tag + " - " + msg + " : " + tr.getMessage());
+  }
+
+  @Override
+  public void e(String tag, String msg) {
+    Log.e("RNFLog", tag + " - " + msg);
+  }
+
+  @Override
+  public void e(String tag, String msg, Throwable tr) {
+    Log.e("RNFLog", tag + " - " + msg + " : " + tr.getMessage());
+  }
+
+  @Override
+  public void wtf(String tag, String msg) {
+    Log.wtf("RNFLog", tag + " - " + msg);
+  }
+
+  @Override
+  public void wtf(String tag, String msg, Throwable tr) {
+    Log.wtf("RNFLog", tag + " - " + msg + " : " + tr.getMessage());
+  }
+
+  @Override
+  public void log(int priority, String tag, String msg) {
+    Log.i("RNFLog", tag + " - " + msg);
+  }
+
+  private class MarkerListener implements ReactMarker.MarkerListener {
+    @Override
+    public void logMarker(ReactMarkerConstants name, @Nullable String tag, int instanceKey) {
+      Log.i("ReactMarker", name.toString() + ": " + tag);
+    }
   }
 }
