2a26af9ddf57efc6f62fbb863051938f3d0e5df7
leegeunhyeok <dev.ghlee@gmail.com>
2023-09-23 16:48:10 +0900

    chore: add lint scripts to root

--- packages/internal/__tests__/internal.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const internal = require('..');
-const assert = require('assert').strict;
-
-assert.strictEqual(internal(), 'Hello from internal');
-console.info('internal tests passed');
