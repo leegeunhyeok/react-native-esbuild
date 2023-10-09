3a8846d40b2df851fa5db7a3d5b8991ad965d97b
leegeunhyeok <dev.ghlee@gmail.com>
2023-10-10 02:04:06 +0900

    refactor(cli): update command descriptions

--- packages/cli/lib/cli.ts
+++ packages/cli/lib/cli.ts
@@ -9,12 +9,12 @@ import type { RawArgv } from './types';
 
 const commonOptions = {
   verbose: {
-    describe: 'print all logs',
+    describe: 'Print all logs',
     type: 'boolean',
     default: false,
   },
   'reset-cache': {
-    describe: 'reset transform cache',
+    describe: 'Reset transform cache',
     type: 'boolean',
     default: false,
   },
@@ -25,75 +25,79 @@ export const cli = (): RawArgv | Promise<RawArgv> => {
     .scriptName('rne')
     .version(VERSION)
     .usage('$0 <cmd> [args]')
-    .command('start', 'start bundler with dev server', (yargs) => {
-      yargs
-        .options({
-          host: {
-            describe: 'set the server host',
-            type: 'string',
-            default: 'localhost',
-          },
-          port: {
-            describe: 'set the server port',
-            type: 'number',
-            default: 8081,
-          },
-          ...commonOptions,
-          // dummy options
-          watchFolders: {
-            describe: 'no-op',
-            type: 'array',
-          },
-          assetPlugins: {
-            describe: 'no-op',
-            type: 'array',
-          },
-          sourceExts: {
-            describe: 'no-op',
-            type: 'array',
-          },
-          'max-workers': {
-            describe: 'no-op',
-            type: 'number',
-          },
-          transformer: {
-            describe: 'no-op',
-            type: 'string',
-          },
-        })
-        .strictOptions()
-        .version(false)
-        .help();
-    })
-    .command('serve', 'serve bundle', (yargs) => {
+    .command(
+      'start',
+      'Start the development server for Native(Android, iOS)',
+      (yargs) => {
+        yargs
+          .options({
+            host: {
+              describe: 'Set the server host',
+              type: 'string',
+              default: 'localhost',
+            },
+            port: {
+              describe: 'Set the server port',
+              type: 'number',
+              default: 8081,
+            },
+            ...commonOptions,
+            // dummy options
+            watchFolders: {
+              describe: 'no-op',
+              type: 'array',
+            },
+            assetPlugins: {
+              describe: 'no-op',
+              type: 'array',
+            },
+            sourceExts: {
+              describe: 'no-op',
+              type: 'array',
+            },
+            'max-workers': {
+              describe: 'no-op',
+              type: 'number',
+            },
+            transformer: {
+              describe: 'no-op',
+              type: 'string',
+            },
+          })
+          .strictOptions()
+          .version(false)
+          .help();
+      },
+    )
+    .command('serve', 'Start the development server for Web', (yargs) => {
       yargs
         .options({
           'entry-file': {
             type: 'string',
-            describe: 'set the entry file path',
+            describe: 'Set the entry file path',
             default: DEFAULT_WEB_ENTRY_POINT,
           },
           host: {
-            describe: 'set the server host',
+            describe: 'Set the server host',
             type: 'string',
             default: 'localhost',
           },
           port: {
-            describe: 'set the server port',
+            describe: 'Set the server port',
             type: 'number',
             default: 8081,
           },
           template: {
-            describe: 'set the template html',
+            describe: 'Set the template html',
             type: 'string',
           },
           dev: {
             type: 'boolean',
-            describe: 'set as development environment',
+            describe: 'Set as development environment',
             default: true,
           },
           minify: {
-            describe: 'enable minify',
+            describe: 'Enable minify',
             type: 'boolean',
           },
           ...commonOptions,
@@ -102,99 +106,103 @@ export const cli = (): RawArgv | Promise<RawArgv> => {
         .version(false)
         .help();
     })
-    .command('bundle', 'bundle your application', (yargs) => {
-      yargs
-        .options({
-          platform: {
-            type: 'string',
-            describe: 'set the target platform for resolve modules',
-            choices: ['android', 'ios', 'web'],
-          },
-          'entry-file': {
-            type: 'string',
-            describe: 'set the entry file path',
-            default: DEFAULT_ENTRY_POINT,
-          },
-          'bundle-output': {
-            type: 'string',
-            describe: 'specify the path to store the resulting bundle',
-          },
-          'sourcemap-output': {
-            type: 'string',
-            describe:
-              'specify the path to store the source map file for the resulting bundle',
-          },
-          'assets-dest': {
-            type: 'string',
-            describe:
-              'specify the directory path for storing assets referenced in the bundle',
-          },
-          dev: {
-            type: 'boolean',
-            describe: 'set as development environment',
-            default: true,
-          },
-          minify: {
-            describe: 'enable minify',
-            type: 'boolean',
-          },
-          metafile: {
-            describe: 'export Esbuild metafile to working directory',
-            type: 'boolean',
-            default: false,
-          },
-          ...commonOptions,
-          // dummy options
-          transformer: {
-            describe: 'no-op',
-            type: 'string',
-          },
-          'bundle-encoding': {
-            describe: 'no-op',
-            type: 'string',
-          },
-          'max-workers': {
-            describe: 'no-op',
-            type: 'number',
-          },
-          'sourcemap-sources-root': {
-            describe: 'no-op',
-            type: 'string',
-          },
-          'sourcemap-use-absolute-path': {
-            describe: 'no-op',
-            type: 'boolean',
-          },
-          'unstable-transform-profile': {
-            describe: 'no-op',
-            type: 'string',
-          },
-          'asset-catalog-dest': {
-            describe: 'no-op',
-            type: 'string',
-          },
-          'read-global-cache': {
-            describe: 'no-op',
-            type: 'boolean',
-          },
-          config: {
-            describe: 'no-op',
-            type: 'string',
-          },
-          'generate-static-view-configs': {
-            describe: 'no-op',
-            type: 'boolean',
-          },
-        })
-        .demandOption(['bundle-output', 'platform'])
-        .strictOptions()
-        .version(false)
-        .help();
-    })
+    .command(
+      'bundle',
+      'Build the bundle for the provided JavaScript entry file',
+      (yargs) => {
+        yargs
+          .options({
+            platform: {
+              type: 'string',
+              describe: 'Set the target platform for resolve modules',
+              choices: ['android', 'ios', 'web'],
+            },
+            'entry-file': {
+              type: 'string',
+              describe: 'Set the entry file path',
+              default: DEFAULT_ENTRY_POINT,
+            },
+            'bundle-output': {
+              type: 'string',
+              describe: 'Specify the path to store the resulting bundle',
+            },
+            'sourcemap-output': {
+              type: 'string',
+              describe:
+                'Specify the path to store the source map file for the resulting bundle',
+            },
+            'assets-dest': {
+              type: 'string',
+              describe:
+                'Specify the directory path for storing assets referenced in the bundle',
+            },
+            dev: {
+              type: 'boolean',
+              describe: 'Set as development environment',
+              default: true,
+            },
+            minify: {
+              describe: 'Enable minify',
+              type: 'boolean',
+            },
+            metafile: {
+              describe: 'Export Esbuild metafile to working directory',
+              type: 'boolean',
+              default: false,
+            },
+            ...commonOptions,
+            // dummy options
+            transformer: {
+              describe: 'no-op',
+              type: 'string',
+            },
+            'bundle-encoding': {
+              describe: 'no-op',
+              type: 'string',
+            },
+            'max-workers': {
+              describe: 'no-op',
+              type: 'number',
+            },
+            'sourcemap-sources-root': {
+              describe: 'no-op',
+              type: 'string',
+            },
+            'sourcemap-use-absolute-path': {
+              describe: 'no-op',
+              type: 'boolean',
+            },
+            'unstable-transform-profile': {
+              describe: 'no-op',
+              type: 'string',
+            },
+            'asset-catalog-dest': {
+              describe: 'no-op',
+              type: 'string',
+            },
+            'read-global-cache': {
+              describe: 'no-op',
+              type: 'boolean',
+            },
+            config: {
+              describe: 'no-op',
+              type: 'string',
+            },
+            'generate-static-view-configs': {
+              describe: 'no-op',
+              type: 'boolean',
+            },
+          })
+          .demandOption(['bundle-output', 'platform'])
+          .strictOptions()
+          .version(false)
+          .help();
+      },
+    )
     .command('ram-bundle', '')
-    .command('cache', 'manage transform cache', (yargs) => {
+    .command('cache', 'Cache utilities', (yargs) => {
       yargs
-        .command('clean', 'clear all transform cache')
+        .command('clean', 'Clear all transform cache')
         .demandCommand()
         .strictCommands()
         .version(false);
