fcea8b97c03b5ce21604ba95a17649e727e8d6c1
leegeunhyeok <dev.ghlee@gmail.com>
2023-10-25 21:36:36 +0900

    chore: replace ts-jest to @swc/jest
    - before: ~10s
    - after: ~3s

--- jest.config.ts
+++ jest.config.ts
@@ -1,73 +1,77 @@
-import type { JestConfigWithTsJest } from 'ts-jest';
+import type { Config } from 'jest';
 
-const config: JestConfigWithTsJest = {
+const transform = {
+  '^.+\\.(t|j)sx?$': '@swc/jest',
+};
+
+const config: Config = {
   projects: [
     {
       displayName: '@react-native-esbuild/cli',
-      preset: 'ts-jest',
+      transform,
       testEnvironment: 'node',
       testMatch: ['<rootDir>/packages/cli/**/*.test.ts'],
       setupFilesAfterEnv: ['<rootDir>/test/setup.ts'],
     },
     {
       displayName: '@react-native-esbuild/config',
-      preset: 'ts-jest',
+      transform,
       testEnvironment: 'node',
       testMatch: ['<rootDir>/packages/config/**/*.test.ts'],
       setupFilesAfterEnv: ['<rootDir>/test/setup.ts'],
     },
     {
       displayName: '@react-native-esbuild/core',
-      preset: 'ts-jest',
+      transform,
       testEnvironment: 'node',
       testMatch: ['<rootDir>/packages/core/**/*.test.ts'],
       setupFilesAfterEnv: ['<rootDir>/test/setup.ts'],
     },
     {
       displayName: '@react-native-esbuild/config',
-      preset: 'ts-jest',
+      transform,
       testEnvironment: 'node',
       testMatch: ['<rootDir>/packages/config/**/*.test.ts'],
       setupFilesAfterEnv: ['<rootDir>/test/setup.ts'],
     },
     {
       displayName: '@react-native-esbuild/dev-server',
-      preset: 'ts-jest',
+      transform,
       testEnvironment: 'node',
       testMatch: ['<rootDir>/packages/dev-server/**/*.test.ts'],
       setupFilesAfterEnv: ['<rootDir>/test/setup.ts'],
     },
     {
       displayName: '@react-native-esbuild/internal',
-      preset: 'ts-jest',
+      transform,
       testEnvironment: 'node',
       testMatch: ['<rootDir>/packages/internal/**/*.test.ts'],
       setupFilesAfterEnv: ['<rootDir>/test/setup.ts'],
     },
     {
       displayName: '@react-native-esbuild/plugins',
-      preset: 'ts-jest',
+      transform,
       testEnvironment: 'node',
       testMatch: ['<rootDir>/packages/plugins/**/*.test.ts'],
       setupFilesAfterEnv: ['<rootDir>/test/setup.ts'],
     },
     {
       displayName: '@react-native-esbuild/symbolicate',
-      preset: 'ts-jest',
+      transform,
       testEnvironment: 'node',
       testMatch: ['<rootDir>/packages/symbolicate/**/*.test.ts'],
       setupFilesAfterEnv: ['<rootDir>/test/setup.ts'],
     },
     {
       displayName: '@react-native-esbuild/transformer',
-      preset: 'ts-jest',
+      transform,
       testEnvironment: 'node',
       testMatch: ['<rootDir>/packages/transformer/**/*.test.ts'],
       setupFilesAfterEnv: ['<rootDir>/test/setup.ts'],
     },
     {
       displayName: '@react-native-esbuild/utils',
-      preset: 'ts-jest',
+      transform,
       testEnvironment: 'node',
       testMatch: ['<rootDir>/packages/utils/**/*.test.ts'],
       setupFilesAfterEnv: ['<rootDir>/test/setup.ts'],
